aa3494b762fbc23f6a8d32902edff7087106cad4
abdi2148 <abdiqafar.abas@gmail.com>
2021-05-27 02:41:24 +0200

    Menu done with scene changes

---
+++ ObstacleGame/Assets/Scripts/Menu Scripts/Pointers/PointerEvents.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PointerEvents : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerClickHandler {
+
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color enterColor = Color.white;
+    [SerializeField] private Color downColor = Color.white;
+    [SerializeField] private UnityEvent OnClick = new UnityEvent();
+
+    private MeshRenderer meshRenderer = null;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        meshRenderer.material.color = enterColor;
+        print("Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        meshRenderer.material.color = downColor;
+        print("Exit");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        meshRenderer.material.color = downColor;
+        print("Up");
+    }
+
+}
